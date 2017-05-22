4d2bec9671323e7da7e8d8efc066a2dcc2d810ac
vigorjao <vigor.jao@eitc.com.tw>
2017-05-22 15:57:30 +0800

    proxy 可加入port, 簡單登入機制

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -10,17 +9,15 @@ import { Router } from "@angular/router";
 export class LoginComponent implements OnInit {
 
   user: any = {};
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
   onLogin() {
     console.log(this.user.username + " " + this.user.password)
-    //this.router.navigate(["home"]);
-    this.userService.onLogin(this.user.username).subscribe(userInfo =>
+    this.userService.onLogin(this.user.username,this.user.password).subscribe(userInfo =>
       console.log(userInfo)
-
     )
   }
 
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,16 +1,31 @@
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http,private router: Router) { }
 
-  onLogin(account: string) {
-    debugger
-    return this.http.get('/api/user/' + account)
-      .map(res => res.json());
+  onLogin(account: string,password:string) {
+    let header: Headers = new Headers();
+    let date : Date = new Date();
+    console.log(date.toLocaleString())
+    header.append("Authorization", "Basic username:" + btoa("test"));
+    header.append("Datetime", "date.toLocaleString()");
+    header.append("Account", account);
+    header.append("Password", password);
+    header.append("Content-Type", "application/jaon");
+    let options = new RequestOptions({ headers: header });
+    //let options = new RequestOptions({ method:"",headers: headers, });
+    return this.http.get('/api/user', options)
+      .map(res =>{
+        debugger
+         res.json()
+         if( res.json() != null )
+         this.router.navigate(["home"]);
+        });
   }
 
 }
