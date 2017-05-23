b4b0b27a2773d8194419af37584b5512005adc3f
vigorjao <vigor.jao@eitc.com.tw>
2017-05-23 18:05:29 +0800

    加入測試資料

--- src/app/home/textile/textile.component.ts
+++ src/app/home/textile/textile.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextileComponent implements OnInit {
 
+  textileInfo: TextileInfo[];
   constructor() { }
 
   ngOnInit() {
+    this.textileInfo = [
+      { TextileName: "CVC", TextileDetail: [{ Color: "red", Amount: 10 }, { Color: "black", Amount: 10 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }, { Color: "black", Amount: 10 }, { Color: "black", Amount: 10 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }, { Color: "black", Amount: 10 }, { Color: "black", Amount: 10 }, { Color: "black", Amount: 10 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] },
+      { TextileName: "TC", TextileDetail: [{ Color: "red", Amount: 2 }, { Color: "black", Amount: 3 }] }]
   }
 
+}
+
+class TextileInfo {
+  TextileName: string;
+  TextileDetail: TextileDetail[];
+}
+class TextileDetail {
+  Color: string;
+  Amount: number;
 }
