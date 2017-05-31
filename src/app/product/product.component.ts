2acfe508816f7de065fb69132aa85f8107918775
vigorjao <vigor.jao@eitc.com.tw>
2017-05-31 22:49:41 +0800

    New feature,Fix
    建立新增product
    修改 textile 表單大小
    修改 textile add 按鈕排版

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    var productInfo = [{ ProductName: '菜瓜布' }, { ProductName: 'TC1*1' }]
+    // var productInfo = [{ ProductName: '菜瓜布' }, { ProductName: 'TC1*1' }]
+    // this.productService.onAddProduct(productInfo).subscribe(info => console.log(info));
+  }
+
+  onAddProduct(productName: string) {
+    var productInfo = [{ ProductName: productName }];
+    console.log(productName);
     this.productService.onAddProduct(productInfo).subscribe(info => console.log(info));
   }
 
