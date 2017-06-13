ea11f29f40456ef4289dd223a545fde2ad66e965
vigorjao <vigor.jao@eitc.com.tw>
2017-06-13 22:23:42 +0800

    New feature
    Dropdown mouseove or leave 可自動開合
    navmenu 可點擊他處收合

--- src/app/navmenu/navmenu.component.ts
+++ src/app/navmenu/navmenu.component.ts
@@ -1,21 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { AuthenticationService } from "app/services/authentication.service";
 
 @Component({
   selector: 'app-navmenu',
   templateUrl: './navmenu.component.html',
+  host: {
+    '(document:click)': 'handleClick($event)',
+  },
   styleUrls: ['./navmenu.component.css']
 })
 export class NavmenuComponent implements OnInit {
   isLogin: boolean = false;
-  constructor(private authService: AuthenticationService) { }
+  public elementRef;
+  isIn = false;   // store state
+  textileIsOpen = false;
+  constructor(private authService: AuthenticationService, myElement: ElementRef) {
+    this.elementRef = myElement;
+  }
 
   ngOnInit() {
     this.authService.tokenEmit.subscribe(token => this.isLogin = token != null);
   }
-  isIn = false;   // store state
+
   toggleState() { // click handler
     let bool = this.isIn;
     this.isIn = bool === false ? true : false;
+  }
+
+  over() {
+    this.textileIsOpen = true;
+  }
+
+  leave() {
+    this.textileIsOpen = false;
+  }
+
+  handleClick(event) {
+    var clickedComponent = event.target;
+    var inside = false;
+    do {
+      if (clickedComponent === this.elementRef.nativeElement) 
+      {
+        inside = true;
+      }
+      clickedComponent = clickedComponent.parentNode;
+    } while (clickedComponent && inside == false);
+
+    if (inside) 
+    {    } 
+    else 
+    {
+      this.isIn = false;
+    }
   }
 }
