2a7de6a706da7c9e34704ba2ac4d4b1617e8d150
vigorjao <vigor.jao@eitc.com.tw>
2017-06-14 00:32:34 +0800

    New feature
    新增搜尋功能

--- src/app/textile/textile-all/textile-all.component.ts
+++ src/app/textile/textile-all/textile-all.component.ts
@@ -1,22 +1,40 @@
+import { TextileInfo } from './../../models/textileInfo';
 import { TextileService } from './../../services/textile.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-textile-all',
   templateUrl: './textile-all.component.html',
   styleUrls: ['./textile-all.component.css']
 })
-export class TextileAllComponent implements OnInit {
+export class TextileAllComponent implements OnInit,OnChanges {
 
-  textileInfo;
+  textileInfo: TextileInfo[] = [];
+  searchTextileInfo: TextileInfo[] = [];
   constructor(private alltextileInfo: TextileService) { }
 
   ngOnInit() {
-    this.alltextileInfo.onGetTextile().subscribe(Info => this.textileInfo = Info, (err) => {
+    this.alltextileInfo.onGetTextile().subscribe(Info => {
+      this.textileInfo = Info;
+      this.searchTextileInfo = Info;
+    }, (err) => {
       if (err === 'Unauthorized') {
         alert('Unauthorized')
       }
     });
+
+  }
+  ngOnChanges(){
+    console.log("test");
+  }
+
+  onSearch(keyword: string) {
+    if (keyword != null) {
+      this.textileInfo = this.searchTextileInfo.filter(a => a.textileName.includes(keyword));
+    }else
+    {
+      this.textileInfo = this.searchTextileInfo;
+    }
   }
 
 }
